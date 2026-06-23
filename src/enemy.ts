2261eb0ae2d07721f4ab290d3cd43509d2ff2896
Kanako Ueba <anpanman.u.k@gmail.com>
2026-06-23 20:19:14 +0900

    第3回課題完了

---
+++ src/enemy.ts
@@ -0,0 +1,38 @@
+import { Character } from "./character";
+
+export class Enemy extends Character {
+  //TODO: maxHp プロパティを追加する
+
+  protected maxHp: number;
+
+  //TODO: maxHp プロパティの初期化
+  constructor(name: string, hp: number, maxHp: number) {
+    super(name, hp);
+    this.maxHp = maxHp;
+  }
+
+  //TODO: maxHp に対する現在の hp の割合を返す getHpRatio メソッドを追加する
+  getHpRatio(): number {
+    const ratio: number = (this.hp / this.maxHp) * 100;
+    return ratio;
+  }
+
+  //TODO: ダメージを受ける takeDamage メソッドを追加する
+  takeDamage(damage: number): void {
+    this.hp -= damage;
+    if (this.hp < 0) {
+      this.hp = 0;
+    }
+  }
+
+  //TODO: 残り HP が 30% 以下の場合、行動を変化させる
+  // 通常時 : name + "は攻撃してきた！"
+  // 残り HP が 30% 以下 : name + "は必死に抵抗している！"
+  attack(): void {
+    if (this.getHpRatio() <= 30) {
+      console.log(`${this.name}は必死に抵抗している！`);
+    } else {
+      console.log(`${this.name}は攻撃してきた!`);
+    }
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,14 @@ warrior.attack();
 const mage = new Mage("メディア", 80);
 mage.showStatus();
 mage.attack();
+
+/第3回課題/;
+
+import { Enemy } from "./enemy";
+
+const slime = new Enemy("スライム", 50, 50);
+slime.showStatus();
+slime.attack();
+slime.takeDamage(40);
+slime.showStatus();
+slime.attack();
